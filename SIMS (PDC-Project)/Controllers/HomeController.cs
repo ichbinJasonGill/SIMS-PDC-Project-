54203e17be683a978d7e06c2cc598c4b5e3b6b0f
Ahsan-Malik-0 <m.ahsan.malik951@gmail.com>
2025-06-18 16:22:40 +0500

    Add new function

--- SIMS (PDC-Project)/Controllers/HomeController.cs
+++ SIMS (PDC-Project)/Controllers/HomeController.cs
@@ -13,15 +13,9 @@ namespace SIMS__PDC_Project_.Controllers
             return View();
         }
 
-<<<<<<< HEAD
-//<<<<<<< HEAD
-        //Hello This is added by Jason Gill at 9:19
-
-//=======
-        // My name is Muhammad Ahsan
-//>>>>>>> 56a94a7d8ca25a94398c860aed2a77d379c86c02
-=======
-        // My name is Muhammad Ahsan
->>>>>>> 55535c082779abb4b762bbaab172827b7c14063f
+        public ActionResult Home()
+        {
+            return View();
+        }
     }
 }
