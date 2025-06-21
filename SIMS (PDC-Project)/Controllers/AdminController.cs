9512c1503d188c145aa986bfb97f74ee770856a7
ichbinJasonGill <ichbinjasongill@gmail.com>
2025-06-21 14:30:26 +0500

    Admin Dashboard in Progress

--- SIMS (PDC-Project)/Controllers/AdminController.cs
+++ SIMS (PDC-Project)/Controllers/AdminController.cs
@@ -1,22 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using SIMS__PDC_Project_.Models;
+using SIMS__PDC_Project_.ViewModel;
+using Supabase_Example.Services;
 
 namespace SIMS__PDC_Project_.Controllers
 {
     public class AdminController : Controller
     {
+
+        private readonly Supabase_DB_Services _supabaseClient = new Supabase_DB_Services(); 
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult AdminDashboard()
+        public async Task<ActionResult> AdminDashboard()
         {
-            return View();
+            List<Advisor> AllAdvisors = new List<Advisor>();
+            List<Student> AllStudents = new List<Student>();
+
+            string studenterror, advisorError;
+
+            (AllStudents, studenterror) = await _supabaseClient.GetAsync<Student>("student");
+
+            if (!string.IsNullOrEmpty(studenterror))
+            {
+                TempData["StudentErrorMessage"] = "Error loading student: " + studenterror;
+                return View(new List<Student>());
+                //return View();
+                //return RedirectToAction("Index");
+            }
+
+
+
+            (AllAdvisors, advisorError) = await _supabaseClient.GetAsync<Advisor>("advisor");
+
+            if (!string.IsNullOrEmpty(advisorError))
+            {
+                TempData["AdvisorErrorMessage"] = "Error loading student: " + advisorError;
+                return View(new List<Student>());
+                //return View();
+                //return RedirectToAction("Index");
+            }
+
+            var AllUsers = new Student_Teacher
+            {
+                Students = AllStudents,
+                Advisors = AllAdvisors
+            };
+
+            return View(AllUsers);
         }
+
     }
 }
---
+++ SIMS (PDC-Project)/ViewModel/Student Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SIMS__PDC_Project_.Models;
+
+namespace SIMS__PDC_Project_.ViewModel
+{
+    public class Student_Teacher
+    {
+        public List<Student> Students { get; set; }
+        public List<Advisor> Advisors { get; set; }
+    }
+}
